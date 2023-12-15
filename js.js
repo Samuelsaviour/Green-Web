323fa3a9bf7dad0411fc5ddc811bd8c85a5caef8
Samuelsaviour <125797889+Samuelsaviour@users.noreply.github.com>
2023-12-15 15:53:28 +0000

    upload

--- js.js
+++ js.js
@@ -1,7 +1,22 @@
 const menuBar=document.querySelector("#menu")
 const sidebar=document.querySelector("#sidebar")
 const header=document.querySelector("#header")
-
+const imageWrapper=document.querySelector("#img-wrapper")
+const rightbtn=document.querySelector("#btn-right")
+const leftbtn=document.querySelector("#btn-left")
+console.log(imageWrapper);
+// initialing a count
+let count =0;
+leftbtn.addEventListener("click",function() {
+  count=count < 4 ? count + 1 :4;
+  imageWrapper.style.transform=`translateX(${count*-300}px)`
+  console.log(count);
+})
+rightbtn.addEventListener("click",function() {
+  count=count > 3 ? count - 1 :0;
+  imageWrapper.style.transform=`translateX(${count*-300}px)`
+  console.log(count);
+})
 // when click let the sidebar slide in
 menuBar.addEventListener("click",function() {
     sidebar.classList.toggle("slide-in")
